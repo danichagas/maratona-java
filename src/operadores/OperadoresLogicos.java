a3275945be8afa8ee169d63cc3bff96062e35299
danichagas <chagasdaniel788@gmail.com>
2024-09-30 14:18:29 -0300

    Operadores Logicos

---
+++ src/operadores/OperadoresLogicos.java
@@ -0,0 +1,11 @@
+package operadores;
+
+public class OperadoresLogicos {
+    public static void main(String[] args) {
+        // AND --> &&
+        // OR --> ||
+        // NOT --> !
+        boolean exemploRelacional = 89 > 90 && 90 > 91;
+        System.out.println(exemploRelacional);
+    }
+}
