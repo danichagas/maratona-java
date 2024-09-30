154c92a4fbb2ff8ee9442f6ba34a668a9d9ec841
danichagas <chagasdaniel788@gmail.com>
2024-09-30 17:07:02 -0300

    IF

---
+++ src/condicionais/If.java
@@ -0,0 +1,11 @@
+package condicionais;
+
+public class If {
+    public static void main(String[] args) {
+        int idade = 17;
+
+        if (idade < 18) {
+            System.out.println("Menor de idade!");
+        }
+    }
+}
