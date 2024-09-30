8e70c565e331cdb5057e4eabf833a188a9be40ce
danichagas <chagasdaniel788@gmail.com>
2024-09-30 14:18:19 -0300

    Aritimeticos

---
+++ src/operadores/OperadoresAritimeticos.java
@@ -0,0 +1,21 @@
+package operadores;
+
+public class OperadoresAritimeticos {
+    public static void main(String[] args) {
+        // + - / *
+        int numero1 = 30;
+        int numero2 = 90;
+        System.out.println(numero1 * numero2);
+
+        // %
+        int resto = 31 % 2;
+        System.out.println(resto);
+
+        // < > <= >= == != ===
+        boolean exemplo1 = 7 > 8;
+        System.out.println(exemplo1);
+
+        boolean exemplo2 = 9 == 9;
+        System.out.println(exemplo2);
+    }
+}
