f4e0ed221cf8a90ff4ebf59d9ce20dcd8314abd1
danichagas <chagasdaniel788@gmail.com>
2024-09-30 14:18:07 -0300

    Exercicio sobre variáveis

---
+++ src/tiposPrimitivos/Exercicio.java
@@ -0,0 +1,13 @@
+package tiposPrimitivos;
+
+public class Exercicio {
+    public static void main(String[] args) {
+        String name = "Daniel Chagas";
+        String adress = "Valle Jardim Castanhal";
+        double salary = 4000.0;
+        String date = "29/09/2024";
+
+        System.out.println("Eu " + name + ", morando na " + adress + ", confirmo que recebi o salário de " +
+                salary + ", na data " + date);
+    }
+}
