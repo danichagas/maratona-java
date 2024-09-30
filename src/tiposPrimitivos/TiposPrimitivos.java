3478f8027abf3916812762a592461e87e122fd71
danichagas <chagasdaniel788@gmail.com>
2024-09-30 14:17:54 -0300

    Tipos em Java

---
+++ src/tiposPrimitivos/TiposPrimitivos.java
@@ -0,0 +1,18 @@
+package tiposPrimitivos;
+
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+
+        String name = "Daniel Conceição Chagas";
+        int age = 17;
+        // Número bastante grande
+        long salary = 10000;
+        double height = 1.84;
+        boolean isTrue = true;
+        char character = 87;
+
+        System.out.println("Meu nome é " + name);
+        System.out.println("Minha idade é " + age);
+        System.out.println("E recebo " + salary + " reais (em nome de Jesus)");
+    }
+}
