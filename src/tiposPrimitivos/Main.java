4d20ec307e5267204eca12798233cba2f8c6ef32
danichagas <chagasdaniel788@gmail.com>
2024-09-30 14:17:41 -0300

    Comentários

---
+++ src/tiposPrimitivos/Main.java
@@ -0,0 +1,16 @@
+package tiposPrimitivos;
+
+/**
+ * Isso é um comentário <b>javadoc</b>
+ */
+public class Main {
+    public static void main(String[] args) {
+        // Comentário aqui
+
+        /*
+        Comentário de múltiplas linhas
+         */
+
+        System.out.println("Hello World!");
+    }
+}
