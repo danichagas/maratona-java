bfde467f026cb3ef25892974f93d242747cd3fe1
danichagas <chagasdaniel788@gmail.com>
2024-10-01 15:52:59 -0300

    SWITCH CASE exercicio

---
+++ src/condicionais/Exercicio.java
@@ -0,0 +1,35 @@
+package condicionais;
+
+public class Exercicio {
+    public static void main(String[] args) {
+        int value = 1;
+
+        switch (value) {
+            case 1 :
+                System.out.println("Final de semana");
+                break;
+            case 2:
+                System.out.println("Dia de semana");
+                break;
+            case 3:
+                System.out.println("Dia de semana");
+                break;
+            case 4:
+                System.out.println("Dia de semana");
+                break;
+            case 5:
+                System.out.println("Dia de semana");
+                break;
+            case 6:
+                System.out.println("Dia de semana");
+                break;
+            case 7:
+                System.out.println("Final de semana");
+                break;
+            default:
+                System.out.println("Valor inválido");
+                break;
+        }
+
+    }
+}
