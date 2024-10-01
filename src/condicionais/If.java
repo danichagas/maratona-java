811d6f2b528b1d3ddf30717c053e73d2cbd0f8dc
danichagas <chagasdaniel788@gmail.com>
2024-10-01 00:26:55 -0300

    ELSE IF

--- src/condicionais/If.java
+++ src/condicionais/If.java
@@ -4,10 +4,12 @@ public class If {
     public static void main(String[] args) {
         int idade = 17;
 
-        if (idade < 18) {
-            System.out.println("Menor de idade!");
+        if (idade < 15) {
+            System.out.println("Categoria Infantil");
+        } else if (idade >= 15 && idade < 18) {
+            System.out.println("Categoria Juvenil");
         } else {
-            System.out.println("Maior de idade!");
+            System.out.println("Categoria Adulta");
         }
     }
 }
