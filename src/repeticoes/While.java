22ca716c480ec35b8f1a2a766f6a04b8fdd2935d
danichagas <chagasdaniel788@gmail.com>
2024-10-01 23:17:10 -0300

    DO WHILE

--- src/repeticoes/While.java
+++ src/repeticoes/While.java
@@ -3,9 +3,14 @@ package repeticoes;
 public class While {
     public static void main(String[] args) {
         int count = 0;
-        while (count <= 10) {
+//        while (count <= 10) {
+//            System.out.println(count);
+//            count++;
+//        }
+
+        do {
             System.out.println(count);
             count++;
-        }
+        } while (count <= 10);
     }
 }
