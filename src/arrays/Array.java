8ea4683e3b791c92ef2d64965260e578ae6ce308
danichagas <chagasdaniel788@gmail.com>
2024-10-03 13:41:44 -0300

    Arrays em Strings

--- src/arrays/Array.java
+++ src/arrays/Array.java
@@ -5,5 +5,14 @@ public class Array {
         int[] idades = new int[5];
         idades[0] = 17;
         System.out.println(idades[0]);
+
+        String[] nomes = new String[3];
+        nomes[0] = "Daniel";
+        nomes[1] = "Silviane";
+        nomes[2] = "Naldo";
+
+        for (int i = 0; i < nomes.length; i++) {
+            System.out.println(nomes[i]);
+        }
       }
 }
