81a24bb2cbb2e83149effb6088202244640e8bb7
danichagas <chagasdaniel788@gmail.com>
2024-10-03 00:19:38 -0300

    Entendendo um Array

---
+++ src/arrays/Array.java
@@ -0,0 +1,9 @@
+package arrays;
+
+public class Array {
+    public static void main(String[] args) {
+        int[] idades = new int[5];
+        idades[0] = 17;
+        System.out.println(idades[0]);
+      }
+}
