6b4b90c37b8084151308a871dfa6ba587cf72c11
danichagas <chagasdaniel788@gmail.com>
2024-10-04 00:04:41 -0300

    Arrays multidimensionais

---
+++ src/arrays/ArrayMultidimensioanais.java
@@ -0,0 +1,8 @@
+package arrays;
+
+public class ArrayMultidimensioanais {
+    public static void main(String[] args) {
+        int[][] meses = new int[12][12];
+        System.out.println(meses[0][0]);
+    }
+}
