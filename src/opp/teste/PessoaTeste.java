149ada172b32ffa7456db2c8255d830a1783f0af
danichagas <chagasdaniel788@gmail.com>
2024-10-05 15:37:39 -0300

    OPP -> Criando outro objeto

--- src/opp/teste/PessoaTeste.java
+++ src/opp/teste/PessoaTeste.java
@@ -6,13 +6,25 @@ public class PessoaTeste {
     public static void main(String[] args) {
 
         Pessoa pessoa = new Pessoa();
+        Pessoa pessoa2 = new Pessoa();
+
         pessoa.nome = "Daniel Chagas";
         pessoa.idade = 17;
-        pessoa.sexo = "M";
+        pessoa.sexo = "Masculino";
+
+        pessoa2.nome = "Naldo Chagas";
+        pessoa2.idade = 58;
+        pessoa2.sexo = "Masculino";
 
         System.out.println(pessoa.nome);
         System.out.println(pessoa.idade);
         System.out.println(pessoa.sexo);
+
+        System.out.println("------------------");
+
+        System.out.println(pessoa2.nome);
+        System.out.println(pessoa2.idade);
+        System.out.println(pessoa2.sexo);
 
     }
 }
